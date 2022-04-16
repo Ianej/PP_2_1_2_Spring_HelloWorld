ae5a8088629d4ce4931494509a60b8edba1a2365
Evgeniy Bondarev <ianej_1@mail.ru>
2022-04-16 17:43:23 +0300

    2.1.2 completed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,10 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld helloWorld = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("bean == helloWorld: " + (bean == helloWorld));
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println("cat == cat1: " + (cat == cat1));
     }
 }
